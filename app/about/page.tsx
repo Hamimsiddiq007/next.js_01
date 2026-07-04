54eb42e22b03368e403a797bfd826cc525218926
Hamim <hamimsiddiq915@gmail.com>
2026-07-04 08:18:37 +0600

    generateStaticParams

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,11 @@
-export default function AboutPage() {
-    // console.log("About server component");
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+    const cookieStore = await cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
     
+    // console.log("About server component");
     return (
         <>
             <h1>About Page</h1>
